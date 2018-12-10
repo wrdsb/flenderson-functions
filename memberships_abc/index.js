71cd06fa3f4c61eb617e870196135ecdc89218e0
James Schumann <jschumann@gmail.com>
2018-12-10 09:46:42 -0500

    active, onleave, etc

--- memberships_abc/index.js
+++ memberships_abc/index.js
@@ -10,6 +10,7 @@ module.exports = function (context, data) {
     var container = 'groups-memberships-ipps-now';
     var rows = context.bindings.iamwpRaw;
     var excluded_job_codes = ['6106', '6118'];
+    var activity_codes = ['ACTIVE', 'ONLEAVE'];
 
     var members = {};
     var public_members = {};
@@ -25,180 +26,184 @@ module.exports = function (context, data) {
     var s4s_job_codes = ['5130', '5130SEPU', '5130TEP', '5131', '5131TEVP', '5152', '5221S', '5230', '5230SSP', '5230TSP', '5231', '5231SSVP', '5231SSVU', '5231TSVP', '8126SERT', '8127SERT', '8128SERT'];
 
     rows.forEach(function(row) {
-        // If we're missing required fields, bail
-        if (
-                (!row.EMAIL_ADDRESS || !row.JOB_CODE || !row.EMP_GROUP_CODE || !row.LOCATION_CODE || !row.PANEL || !row.SCHOOL_CODE || !row.ACTIVITY_CODE)
-                || excluded_job_codes.includes(row.JOB_CODE)
-                || !isNaN(row.SCHOOL_CODE)
-            ) {
-            return;
-        }
+        if (row.EMAIL_ADDRESS
+            && row.JOB_CODE
+            && row.EMP_GROUP_CODE
+            && row.LOCATION_CODE
+            && row.PANEL
+            && row.SCHOOL_CODE
+            && row.ACTIVITY_CODE
+            && !excluded_job_codes.includes(row.JOB_CODE)
+            && activity_codes.includes(row.ACTIVITY_CODE)
+            && isNaN(row.SCHOOL_CODE)
+        ) {
 
-        var email = row.EMAIL_ADDRESS;
-        var job_code = row.JOB_CODE;
-        var group_code = row.EMP_GROUP_CODE;
-        var location_code = row.LOCATION_CODE;
-        var panel = row.PANEL;
-        var school_code = row.SCHOOL_CODE.toLowerCase();
-        var activity_code = row.ACTIVITY_CODE;
+            var email = row.EMAIL_ADDRESS;
+            var job_code = row.JOB_CODE;
+            var group_code = row.EMP_GROUP_CODE;
+            var location_code = row.LOCATION_CODE;
+            var panel = row.PANEL;
+            var school_code = row.SCHOOL_CODE.toLowerCase();
+            var activity_code = row.ACTIVITY_CODE;
 
-        if (!members[school_code]) {
-            members[school_code] = {};
+            if (!members[school_code]) {
+                members[school_code] = {};
+            
+                members[school_code]['staff'] = {};
+                members[school_code]['staff-discussion'] = {};
+                members[school_code]['admin'] = {};
+                members[school_code]['attendance'] = {};
+                members[school_code]['beforeafter'] = {};
+                members[school_code]['easyconnect'] = {};
+                members[school_code]['itunes'] = {};
+                members[school_code]['office'] = {};
+                members[school_code]['orders'] = {};
+                members[school_code]['s4s'] = {};
+                members[school_code]['stswr'] = {};
+                members[school_code]['its'] = {};
+            }
         
-            members[school_code]['staff'] = {};
-            members[school_code]['staff-discussion'] = {};
-            members[school_code]['admin'] = {};
-            members[school_code]['attendance'] = {};
-            members[school_code]['beforeafter'] = {};
-            members[school_code]['easyconnect'] = {};
-            members[school_code]['itunes'] = {};
-            members[school_code]['office'] = {};
-            members[school_code]['orders'] = {};
-            members[school_code]['s4s'] = {};
-            members[school_code]['stswr'] = {};
-            members[school_code]['its'] = {};
-        }
-        
-        if (!public_members[school_code]) {
-            public_members[school_code] = {};
-        }
+            if (!public_members[school_code]) {
+                public_members[school_code] = {};
+            }
 
-        if (admin_job_codes.includes(job_code)) {
-            members[school_code]['staff'][email] = {
-                email:          email,
-                role:           "MANAGER",
-                status:         "ACTIVE",
-                type:           "USER",
-                groupKey:       school_code + '-staff@wrdsb.ca'
-            };
-        } else {
-            members[school_code]['staff'][email] = {
-                email:          email,
-                role:           "MEMBER",
-                status:         "ACTIVE",
-                type:           "USER",
-                groupKey:       school_code + '-staff@wrdsb.ca'
-            };
-        }
+            if (admin_job_codes.includes(job_code)) {
+                members[school_code]['staff'][email] = {
+                    email:          email,
+                    role:           "MANAGER",
+                    status:         "ACTIVE",
+                    type:           "USER",
+                    groupKey:       school_code + '-staff@wrdsb.ca'
+                };
+            } else {
+                members[school_code]['staff'][email] = {
+                    email:          email,
+                    role:           "MEMBER",
+                    status:         "ACTIVE",
+                    type:           "USER",
+                    groupKey:       school_code + '-staff@wrdsb.ca'
+                };
+            }
     
-        if (office_job_codes.includes(job_code)) {
-            members[school_code]['staff-discussion'][email] = {
-                email:          email,
-                role:           "MANAGER",
-                status:         "ACTIVE",
-                type:           "USER",
-                groupKey:       school_code + '-staff-discussion@wrdsb.ca'
-            };
-        }
+            if (office_job_codes.includes(job_code)) {
+                members[school_code]['staff-discussion'][email] = {
+                    email:          email,
+                    role:           "MANAGER",
+                    status:         "ACTIVE",
+                    type:           "USER",
+                    groupKey:       school_code + '-staff-discussion@wrdsb.ca'
+                };
+            }
         
-        if (admin_job_codes.includes(job_code)) {
-            members[school_code]['admin'][email] = {
-                email:          email,
-                role:           "MEMBER",
-                status:         "ACTIVE",
-                type:           "USER",
-                groupKey:       school_code + '-admin@wrdsb.ca'
-            };
-        }
+            if (admin_job_codes.includes(job_code)) {
+                members[school_code]['admin'][email] = {
+                    email:          email,
+                    role:           "MEMBER",
+                    status:         "ACTIVE",
+                    type:           "USER",
+                    groupKey:       school_code + '-admin@wrdsb.ca'
+                };
+            }
         
-        if (attendance_job_codes.includes(job_code)) {
-            members[school_code]['attendance'][email] = {
-                email:          email,
-                role:           "MEMBER",
-                status:         "ACTIVE",
-                type:           "USER",
-                groupKey:       school_code + '-attendance@wrdsb.ca'
-            };
-        }
+            if (attendance_job_codes.includes(job_code)) {
+                members[school_code]['attendance'][email] = {
+                    email:          email,
+                    role:           "MEMBER",
+                    status:         "ACTIVE",
+                    type:           "USER",
+                    groupKey:       school_code + '-attendance@wrdsb.ca'
+                };
+            }
         
-        if (beforeafter_job_codes.includes(job_code)) {
-            members[school_code]['beforeafter'][email] = {
-                email:          email,
-                role:           "MEMBER",
-                status:         "ACTIVE",
-                type:           "USER",
-                groupKey:       school_code + '-beforeafter@wrdsb.ca'
-            };
-        }
+            if (beforeafter_job_codes.includes(job_code)) {
+                members[school_code]['beforeafter'][email] = {
+                    email:          email,
+                    role:           "MEMBER",
+                    status:         "ACTIVE",
+                    type:           "USER",
+                    groupKey:       school_code + '-beforeafter@wrdsb.ca'
+                };
+            }
         
-        if (easyconnect_job_codes.includes(job_code)) {
-            members[school_code]['easyconnect'][email] = {
-                email:          email,
-                role:           "MEMBER",
-                status:         "ACTIVE",
-                type:           "USER",
-                groupKey:       school_code + '-easyconnect@wrdsb.ca'
-            };
-        }
+            if (easyconnect_job_codes.includes(job_code)) {
+                members[school_code]['easyconnect'][email] = {
+                    email:          email,
+                    role:           "MEMBER",
+                    status:         "ACTIVE",
+                    type:           "USER",
+                    groupKey:       school_code + '-easyconnect@wrdsb.ca'
+                };
+            }
         
-        if (its_job_codes.includes(job_code)) {
-            members[school_code]['its'][email] = {
-                email:          email,
-                role:           "MEMBER",
-                status:         "ACTIVE",
-                type:           "USER",
-                groupKey:       school_code + '-its@wrdsb.ca'
-            };
-        }
+            if (its_job_codes.includes(job_code)) {
+                members[school_code]['its'][email] = {
+                    email:          email,
+                    role:           "MEMBER",
+                    status:         "ACTIVE",
+                    type:           "USER",
+                    groupKey:       school_code + '-its@wrdsb.ca'
+                };
+            }
         
-        if (admin_job_codes.includes(job_code)) {
-            members[school_code]['itunes'][email] = {
-                email:          email,
-                role:           "MEMBER",
-                status:         "ACTIVE",
-                type:           "USER",
-                groupKey:       school_code + '-itunes@wrdsb.ca'
-            };
-        }
+            if (admin_job_codes.includes(job_code)) {
+                members[school_code]['itunes'][email] = {
+                    email:          email,
+                    role:           "MEMBER",
+                    status:         "ACTIVE",
+                    type:           "USER",
+                    groupKey:       school_code + '-itunes@wrdsb.ca'
+                };
+            }
         
-        if (office_job_codes.includes(job_code)) {
-            members[school_code]['office'][email] = {
-                email:          email,
-                role:           "MEMBER",
-                status:         "ACTIVE",
-                type:           "USER",
-                groupKey:       school_code + '-office@wrdsb.ca'
-            };
-        }
+            if (office_job_codes.includes(job_code)) {
+                members[school_code]['office'][email] = {
+                    email:          email,
+                    role:           "MEMBER",
+                    status:         "ACTIVE",
+                    type:           "USER",
+                    groupKey:       school_code + '-office@wrdsb.ca'
+                };
+            }
         
-        if (orders_job_codes.includes(job_code)) {
-            members[school_code]['orders'][email] = {
-                email:          email,
-                role:           "MEMBER",
-                status:         "ACTIVE",
-                type:           "USER",
-                groupKey:       school_code + '-orders@wrdsb.ca'
-            };
-        }
+            if (orders_job_codes.includes(job_code)) {
+                members[school_code]['orders'][email] = {
+                    email:          email,
+                    role:           "MEMBER",
+                    status:         "ACTIVE",
+                    type:           "USER",
+                    groupKey:       school_code + '-orders@wrdsb.ca'
+                };
+            }
         
-        if (s4s_job_codes.includes(job_code)) {
-            members[school_code]['s4s'][email] = {
-                email:          email,
-                role:           "MEMBER",
-                status:         "ACTIVE",
-                type:           "USER",
-                groupKey:       school_code + '-s4s@wrdsb.ca'
-            };
-        }
+            if (s4s_job_codes.includes(job_code)) {
+                members[school_code]['s4s'][email] = {
+                    email:          email,
+                    role:           "MEMBER",
+                    status:         "ACTIVE",
+                    type:           "USER",
+                    groupKey:       school_code + '-s4s@wrdsb.ca'
+                };
+            }
         
-        if (office_job_codes.includes(job_code)) {
-            members[school_code]['stswr'][email] = {
-                email:          email,
-                role:           "MEMBER",
-                status:         "ACTIVE",
-                type:           "USER",
-                groupKey:       school_code + '-stswr@wrdsb.ca'
-            };
-        }
+            if (office_job_codes.includes(job_code)) {
+                members[school_code]['stswr'][email] = {
+                    email:          email,
+                    role:           "MEMBER",
+                    status:         "ACTIVE",
+                    type:           "USER",
+                    groupKey:       school_code + '-stswr@wrdsb.ca'
+                };
+            }
         
-        if (office_job_codes.includes(job_code)) {
-            public_members[school_code][email] = {
-                email:          email,
-                role:           "MEMBER",
-                status:         "ACTIVE",
-                type:           "USER",
-                groupKey:       school_code + '@wrdsb.ca'
-            };
+            if (office_job_codes.includes(job_code)) {
+                public_members[school_code][email] = {
+                    email:          email,
+                    role:           "MEMBER",
+                    status:         "ACTIVE",
+                    type:           "USER",
+                    groupKey:       school_code + '@wrdsb.ca'
+                };
+            }
         }
     });
 
