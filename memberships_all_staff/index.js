cdb65bd562477a9bce8a510abe00f8fd10699c39
James Schumann <jschumann@gmail.com>
2018-12-11 14:54:19 -0500

    go back to http triggers. we'll use function apps to trigger

--- memberships_all_staff/index.js
+++ memberships_all_staff/index.js
@@ -1,4 +1,4 @@
-module.exports = async function (context) {
+module.exports = function (context) {
     var execution_timestamp = (new Date()).toJSON();  // format: 2012-04-23T18:25:43.511Z
     var rows = context.bindings.iamwpRaw;
 
